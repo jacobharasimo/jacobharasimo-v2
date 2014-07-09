91ef4ad2b32d4ab63be2bc392984a84c72f4c25d
Jacob Harasimo <jacobharasimo@gmail.com>
2014-07-09 16:16:04 -0400

    finished base filters
    basic plumbing done for isotope

--- app/scripts/controllers/MyWork.js
+++ app/scripts/controllers/MyWork.js
@@ -10,6 +10,7 @@
 angular.module('jacobharasimoApp')
   .controller('MyWorkCtrl', function ($scope,projects) {
         $scope.searchTerm="";
+        $scope.filterType="technologies";
         $scope.Projects = projects.listAll(
             function(result){},
             function(error){throw new Exception(error);}
